fadae7b6681c1fc9ea3e6362328831c582dcf21c
Nikesh <nikeshkhatri217@gmail.com>
2022-06-16 23:58:24 +0545

    readme

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -9,10 +10,11 @@ app.use(express.json());
 hbs.registerPartials("./views/partials"); //partials
 app.set("view engine", "hbs"); //view engine configuration
 // database connection
-const dbUrl = "mongodb+srv://nikeshInventoryManagement:HT*cZU5WKG8LEyD@cluster0.2ipv9.mongodb.net/Inventory-management";
+const dbUrl = process.env.dburl;
 mongoose
 	.connect(dbUrl)
 	.then((result) => app.listen(3000))
+	.then(console.log("server running at port 3000"))
 	.catch((error) => console.error("can't connect to Mongoose server at dbUrl " + error.message));
 
 //controllers
